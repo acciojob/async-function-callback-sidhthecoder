8dd8846398ee3f473d60e895141d44db3b94fc7c
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-03-26 18:43:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
+async function getData(callback) {
+		  const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+		  const data = await response.json();
+		  callback(data.title);
+		}
 
+		document.getElementById('btn').addEventListener('click', async () => {
+			getData((title) => {
+				document.getElementById('output').innerText = title;
+			});
+		});
 //your JS code here. If required.
